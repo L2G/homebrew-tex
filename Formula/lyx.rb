06d245e91d4456e4c4114edfcc35042731e90159
Larry Gilbert <larry@l2g.to>
2015-05-06 12:17:36 -0700

    lyx: formula from boilerplate

---
+++ Formula/lyx.rb
@@ -0,0 +1,25 @@
+class Lyx < Formula
+  homepage "http://www.lyx.org/"
+  url "ftp://ftp.lyx.org/pub/lyx/stable/2.1.x/lyx-2.1.3.tar.xz"
+  version "2.1.3"
+  sha256 "2fd52e7e5ee372983eb6b0a7f04765cb0df6f8c1f3420081d9f7761acfc75682"
+
+  # depends_on "cmake" => :build
+  depends_on :x11 # if your formula requires any X11/XQuartz components
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+
+    # Remove unrecognized options if warned by configure
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    # system "cmake", ".", *std_cmake_args
+    system "make", "install" # if this fails, try separate make/make install steps
+  end
+
+  test do
+    system "false"
+  end
+end
