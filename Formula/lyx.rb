e5ca515e5e9b314df2622cd24159da66930ab08a
Larry Gilbert <larry@l2g.to>
2015-05-06 12:50:25 -0700

    lyx: add dependencies on boost and libmagic

--- Formula/lyx.rb
+++ Formula/lyx.rb
@@ -4,18 +4,23 @@ class Lyx < Formula
   version "2.1.3"
   sha256 "2fd52e7e5ee372983eb6b0a7f04765cb0df6f8c1f3420081d9f7761acfc75682"
 
+  depends_on "boost"
+  depends_on "libmagic"
   depends_on "qt"
 
   def install
     ENV["QT4DIR"] = Formula["qt"].prefix
+    ENV.append "CPPFLAGS", "-I" + Formula["boost"].include + "/boost/tr1"
     interactive_shell
+
     # ENV.deparallelize  # if your formula fails when building in parallel
 
     # Remove unrecognized options if warned by configure
     system "./configure", "--disable-debug",
                           "--disable-dependency-tracking",
                           "--disable-silent-rules",
-                          "--prefix=#{prefix}"
+                          "--prefix=#{prefix}",
+                          "--without-included-boost"
     # system "cmake", ".", *std_cmake_args
     system "make", "install" # if this fails, try separate make/make install steps
   end
