a977f6e591e5e07552e8e439a2dabc1d72d8a0ce
Larry Gilbert <larry@l2g.to>
2015-05-06 13:42:07 -0700

    lyx: remove redundant version specifier

--- Formula/lyx.rb
+++ Formula/lyx.rb
@@ -1,7 +1,6 @@
 class Lyx < Formula
   homepage "http://www.lyx.org/"
   url "ftp://ftp.lyx.org/pub/lyx/stable/2.1.x/lyx-2.1.3.tar.xz"
-  version "2.1.3"
   sha256 "2fd52e7e5ee372983eb6b0a7f04765cb0df6f8c1f3420081d9f7761acfc75682"
 
   depends_on :x11
